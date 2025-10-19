f82599bafce4e8cea55c503515ae0af42d382d68
Muhammad Huzaifa <muhammadhuzaifa_mh@yahoo.com>
2025-10-19 21:20:49 +0500

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] orderID = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
+
+        foreach (string id in orderID)
+        {
+            if(id.StartsWith("B"))
+            {
+                System.Console.WriteLine(id);
+            }
+        }
+    }
+}
